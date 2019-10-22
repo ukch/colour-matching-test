5d609b4acfbb28404163489dea6ceb8fefbc7e3f
Joel Cross <ukch@users.noreply.github.com>
2019-10-22 18:10:02 +0100

    Compute euclidian distance between RGB colours

--- colour_matcher.py
+++ colour_matcher.py
@@ -1,5 +1,53 @@
 """The main colour matching module"""
 
+from operator import itemgetter
+
+import numpy as np
+
+# TODO would it be better to use HSV colours instead of RGB?
+
+
+class Colour:
+    def __init__(self, name=None):
+        self.name = name
+        self._rgb = None
+
+    @classmethod
+    def from_rgb(cls, rgb, **kwargs):
+        self = cls(**kwargs)
+        self._rgb = np.array(rgb)
+        return self
+
+    @property
+    def rgb(self):
+        if self._rgb is None:
+            return ValueError("No RGB value")
+        return self._rgb
+
+    def distance_from(self, other):
+        return np.linalg.norm(self.rgb - other.rgb)
+
+
+# TODO add the 16 named colours from the HTML 4.01 specification
+NAMED_COLOURS = frozenset([
+    Colour.from_rgb([000, 000, 000], name="black"),
+    Colour.from_rgb([128, 128, 128], name="grey"),
+    Colour.from_rgb([000, 000, 128], name="navy"),
+    Colour.from_rgb([000, 128, 128], name="teal"),
+])
+
 
 def get_main_colour_from_image(image):
-    return
+    pixel_count = np.product(image.size)
+    colour_list = sorted(image.getcolors(pixel_count),
+                         key=itemgetter(0), reverse=True)
+    main_colour = Colour.from_rgb(colour_list[0][1])
+    distances = {}
+    for colour in NAMED_COLOURS:
+        distance = main_colour.distance_from(colour)
+        if distance == 0:
+            # Colour is exact so no need to keep looking
+            return colour.name
+        else:
+            distances[colour.name] = distance
+    return min(distances.items(), key=itemgetter(1))[0]
